68a84a0759154eabf1b07f20608d1fccd4900af6
MassimilianoMura <massimilianomura1983@gmail.com>
2015-09-01 19:04:06 +0100

    basic gulp-sass configuration done

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
 
+gulp.task('sass', function () {
+  gulp.src('./stylesheets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
 
-gulp.task('default', function() {
-  console.log('Hello teodoro');
+gulp.task('sass:watch', function () {
+  gulp.watch('./stylesheets/**/*.scss', ['sass']);
 });
